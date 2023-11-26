e84e1921ed5c80629641233b081ffa14325c8b70
Marlo Abend <marloabend@aol.de>
2023-11-26 23:07:53 +0100

    Implement auto move

--- src/app/component/ouril/ouril.component.ts
+++ src/app/component/ouril/ouril.component.ts
@@ -6,13 +6,14 @@ import {Variant} from './type/variant.type';
 import {Pit} from './model/pit.model';
 import {PitComponent} from '../pit/pit.component';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-ouril',
   templateUrl: './ouril.component.html',
   styleUrls: ['./ouril.component.scss'],
   standalone: true,
-  imports: [CommonModule, PitComponent]
+  imports: [CommonModule, FormsModule, PitComponent]
 })
 export class OurilComponent {
 
@@ -22,14 +23,34 @@ export class OurilComponent {
   currentPlayer: Player;
   stats: Stats;
   validMoves: number[];
+  autoMovesString = '';
+  autoMoveLoopTime = 500;
+  autoMoveInterval!: any;
 
   constructor() {
     this.initGame();
   }
 
   onMove(pit: Pit): void {
-    this.game.move(pit.getIndex());
-    this.updateStateDisplay();
+    this.stopAutoMove();
+    this.move(pit.getIndex());
+  }
+
+  onPlayAutoMoves(): void {
+    const moves = this.autoMovesString
+      .split(',')
+      .map(indexString => indexString.trim())
+      .filter(indexString => indexString)
+      .map(indexString => +indexString)
+      .filter(index => !isNaN(index));
+
+    if (moves.length) {
+      this.startAutoMove(moves);
+    }
+  }
+
+  onStopAutoMoves(): void {
+    this.stopAutoMove();
   }
 
   onReset(): void {
@@ -37,6 +58,7 @@ export class OurilComponent {
   }
 
   private initGame(): void {
+    this.stopAutoMove();
     this.game = new Ouril(this.variant);
     this.updateStateDisplay();
   }
@@ -49,5 +71,30 @@ export class OurilComponent {
     this.currentPlayer = this.game.getCurrentPlayer();
     this.stats = this.game.getStats();
     this.validMoves = this.game.getValidMoves();
+  }
+
+  private move(index: number): void {
+    this.game.move(index);
+    this.updateStateDisplay();
+  }
+
+  private startAutoMove(moves: number[]): void {
+    if (!moves.length) {
+      return;
+    }
+    this.initGame();
+    let index = 0;
+    this.autoMoveInterval = setInterval(() => {
+      console.warn('auto move (', index + 1, '/', moves.length, ') pit', moves[index]);
+      this.move(moves[index]);
+      index++;
+      if (index >= moves.length) {
+        this.stopAutoMove();
+      }
+    }, this.autoMoveLoopTime);
+  }
+
+  private stopAutoMove(): void {
+    clearInterval(this.autoMoveInterval);
   }
 }
