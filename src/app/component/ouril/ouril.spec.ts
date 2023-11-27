34a15190f013ca11afd10b6943497e81c35a4007
Marlo Abend <marloabend@aol.de>
2023-11-27 01:17:33 +0100

    Include move histories for possible test cases

--- src/app/component/ouril/ouril.spec.ts
+++ src/app/component/ouril/ouril.spec.ts
@@ -38,3 +38,31 @@ describe('Ouril', () => {
     expect(pits[5].getValue()).toEqual(4);
   })
 });
+
+
+/*
+Possbile test cases
+
+A wins
+[53]: 0,6,1,7,2,8,0,10,3,6,4,9,5,11,1,10,2,7,3,8,0,9,4,6,1,11,4,10,2,6,3,9,0,8,4,7,5,10,3,11,4,9,0,11,5,8,2,6,3,10,1,11,4
+[23]: 3,7,4,8,2,9,1,10,2,11,0,7,4,6,3,6,5,10,1,11,2,7,4
+[23]: 2,6,1,9,3,8,0,7,4,10,1,9,2,8,3,10,0,9,1,10,3,11,5 (Exactly with 25 leaving only one possible move)
+
+B wins
+[44] 1,10,0,11,1,9,2,8,3,9,4,7,5,8,1,10,2,11,5,6,4,9,3,7,5,10,1,11,0,6,1,9,3,8,2,10,4,7,1,8,0,9,1,11
+
+A must feed after own move
+A must feed after own move but cant
+
+A must feed after opponent move
+A must feed after opponent move but cant
+
+B must feed after own move
+[30] 2,10,1,11,0,9,3,9,2,8,4,7,2,9,0,6,5,8,3,10,1,11,2,9,3,10,4,7,5,6
+
+B must feed after own move but cant
+[44] 5,11,4,6,0,6,5,7,3,9,2,9,1,7,5,10,3,8,3,11,5,6,4,10,2,7,5,9,1,6,5,6,4,11,5,10,3,9,0,8,4,7,5,11
+
+B must feed after opponent move
+B must feed after opponent move but cant
+*/
